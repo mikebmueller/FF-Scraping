7a5f8c5cec792c0729d2e5e035273e39859532c7
mikebmueller <mikebmueller@icloud.com>
2024-08-06 13:49:37 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
-leagueID = "1609009"
-leagueStartYear = 2013
-leagueEndYear = 2024
+leagueID = "2586569"
+leagueStartYear = 2014
+leagueEndYear = 2022
 
 standings_directory = './output/'+ leagueID + '-history-standings/'
 gamecenter_directory = './output/'+ leagueID + '-history-teamgamecenter/'
