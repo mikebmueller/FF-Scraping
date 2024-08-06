1e1d074ba65f011a520ee4344c3bae6164d4de57
mikebmueller <mikebmueller@icloud.com>
2024-08-06 13:52:35 -0600

    Update cookieString.py

--- cookieString.py
+++ cookieString.py
@@ -1,6 +1,6 @@
 
 # Paste your cookie string here
-cookie_string = ""
+cookie_string = "179643557%7CMCIDTS%7C19942%7CMCMID%7C72901627170543240313289426438946126625%7CMCAID%7CNONE%7CMCOPTOUT-1722981050s%7CNONE%7CvVersion%7C5.5.0"
 
 # Dict to store cookies
 cookies = {}
